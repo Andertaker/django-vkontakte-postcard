e4732433f04c7ee7a9ae1f31653b3102658f4626
krupin.dv <krupin.dv19@gmail.com>
2015-02-04 16:38:53 +0300

    send postcard function

--- vkontakte_postcard/post.py
+++ vkontakte_postcard/post.py
@@ -1,10 +1,43 @@
 # -*- coding: utf-8 -*-
+from django.utils import timezone
+
+from vkontakte_comments.models import Comment
 
 from . models import Postcard
-from django.core.files.storage import FileSystemStorage
 
 
 
+def send_postcard(album):
+    cards = Postcard.objects.filter(comment=None).order_by('id')[:1]
+
+    if not cards:
+        return None
+
+    c = cards[0]
+
+    files = [c.image.file.name]
+    photo = album.upload_photos(files, caption=c.comment_text)[0]
+
+    # senc comment
+    text = u'[id%(receiver_id)s|%(receiver_name)s], эту открытку тебе прислал [id%(sender_id)s|%(sender_name)s]' % {
+        'receiver_id': c.receiver_id,
+        'receiver_name': c.receiver_name,
+        'sender_id': c.sender_id,
+        'sender_name': c.sender_name}
+    comment = Comment(text=text, object=photo, owner=album.owner, date=timezone.now())
+    comment.save(commit_remote=True)
+
+    c.comment = comment
+    c.photo = photo
+    c.save()
+
+    return c
+
+
+
+
+# Mass upload for test purpose
+# dos't set caption for photos
 def upload_postcard_images(album):
 
     #cards = Postcard.objects.filter(photo=None)[:1]
@@ -21,4 +54,18 @@ def upload_postcard_images(album):
 
     for i, c in enumerate(cards):
         c.photo = photos[i]
+
+        # author=user,
+        # create comment
+        text = u'[id%(receiver_id)s|%(receiver_name)s], эту открытку тебе прислал [id%(sender_id)s|%(sender_name)s]' % {
+            'receiver_id': c.receiver_id,
+            'receiver_name': c.receiver_name,
+            'sender_id': c.sender_id,
+            'sender_name': c.sender_name}
+
+        print text
+        comment = Comment(text=text, object=c.photo, owner=album.owner, date=timezone.now())
+        comment.save(commit_remote=True)
+
+        c.comment = comment
         c.save()
--- vkontakte_postcard/views.py
+++ vkontakte_postcard/views.py
@@ -2,22 +2,29 @@
 from django.http import HttpResponse
 
 from vkontakte_groups.models import Group
-from vkontakte_postcard.post import upload_postcard_images
-
 from vkontakte_photos.models import Album, Photo
+from vkontakte_postcard.post import send_postcard
 from vkontakte_users.models import User
 
 
 POSTCARD_GROUP_ID = 59154616
 POSTCARD_ALBUM_ID = 210546417
 
+#POSTCARD_GROUP_ID = 16297716 # cocacola
+#POSTCARD_ALBUM_ID = 187237395 # Есть кто-то, кто очень любит тебя!
+
+
 
 def send_postcards(request):
     group = Group.remote.fetch(ids=[POSTCARD_GROUP_ID])[0]
     album = Album.remote.fetch(owner=group, ids=[POSTCARD_ALBUM_ID])[0]
-    print album
 
-    upload_postcard_images(album)
+    while 1:
+        c = send_postcard(album)
+        if c:
+            print "postcard %s was sended" % c.id
+        else:
+            break;
 
     return HttpResponse("END")
 
@@ -25,8 +32,12 @@ def send_postcards(request):
 def send_user_postcards(request):
     user = User.remote.fetch(ids=[201164356])[0]
     album = Album.remote.fetch(owner=user, ids=[209873101])[0]
-    print album
 
-    upload_postcard_images(album)
+    while 1:
+        c = send_postcard(album)
+        if c:
+            print "postcard %s was sended" % c.id
+        else:
+            break;
 
     return HttpResponse("END")
